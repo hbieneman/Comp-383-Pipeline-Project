519d5abe087e2b3d0b6485724adeb1979d28ca26
hbieneman <160555086+hbieneman@users.noreply.github.com>
2024-03-01 10:59:08 -0600

    Delete OutputDirectory.py

--- OutputDirectory.py
+++
@@ -1,8 +0,0 @@
-import os
-first_name = "Hailey"
-last_name = "Bieneman"
-
-
-output_directory = f"PipelineProject_{first_name}_{last_name}" # piece together name
-os.system(f"mkdir {output_directory}") # making directory for output
-os.chdir(output_directory)
